2a1e38d8f996b610e5efef7290ec99f2ffe533af
MAOUHOUB Hamza <hamzamaouhoub4@gmail.com>
2024-01-09 11:59:46 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+
+
+import { H1 } from './styles/test'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <H1 color='#ba3838'>
+        hello
+      </H1>
+    </>
+  )
+}
+
+export default App
